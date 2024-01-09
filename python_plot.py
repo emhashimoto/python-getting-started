eb1528c8d65dbfeb3af0e94eaa47abc4e5ffd9c5
Elizabeth Mie Hashimoto <emiehashimoto@gmail.com>
2024-01-09 15:33:56 -0300

    commit-plot

--- python_plot.py
+++ python_plot.py
@@ -4,14 +4,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Gráfico
+# Dados
 data = {'a': np.arange(50),
         'c': np.random.randint(0, 50, 50),
         'd': np.random.randn(50)}
 data['b'] = data['a'] + 10 * np.random.randn(50)
 data['d'] = np.abs(data['d']) * 100
 
+# Gráfico de dispersão
 plt.scatter('a', 'b', c='c', s='d', data=data)
 plt.xlabel('entry a')
 plt.ylabel('entry b')
 plt.show()
+
+# Usando o pacote ggplot
+from plotnine.data import mpg
+from plotnine import ggplot, aes, geom_bar
+
+ggplot(mpg) + aes(x='class') + geom_bar(size=20)
