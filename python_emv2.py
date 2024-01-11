3f0bb9b4d61f92de5b156e67c48b65a5d54c01af
Elizabeth Mie Hashimoto <emiehashimoto@gmail.com>
2024-01-11 07:57:28 -0300

    commit-emv2

--- python_emv2.py
+++ python_emv2.py
@@ -7,17 +7,17 @@ from scipy import optimize
 import sys
 
 # Gerar dadosS
-x = np.random.normal(10,1,100)
+x = np.random.normal(10,2,100)
 
 # Logartimo da função de verossimilhança
 def logvero(theta,x):
   n = len(x)
   mu = theta[0]
-  s2 = theta[1]
+  s = theta[1]  # usando sigma, o método converge para qualquer valor inicial
   soma = sum((x-mu)**2)
-  lv = -((n/2)*math.log(2*math.pi))-((n/2)*math.log(s2))-(soma/(2*s2))
+  lv = -((n/2)*math.log(2*math.pi))-((n/2)*math.log(s**2))-(soma/(2*(s**2)))
   return -lv
 
-theta0 = [10, 1]
+theta0 = [0.1, 0.1]
 out = optimize.minimize(logvero, theta0, args=(x), method='bfgs')
 print(out)
